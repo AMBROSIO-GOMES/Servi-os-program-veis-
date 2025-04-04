ad44a8848f5d7c005ac54175ce9be91d9c224fe1
AMBROSIO-GOMES <hosiogomes@gmail.com>
2025-04-04 22:08:22 +0100

    Add files via upload

---
+++ fon.js
@@ -0,0 +1,12 @@
+class fon{
+    constructor(mobileMenu,navLinks,navList){
+       this.mobileMenu= document.querySelector(mobileMenu) ;
+       this.navList=document.querySelector(this.navList) ;
+       this.navLinks=document.querySelector(this.navLinks) ;
+       this.activeClass= "active";
+    }
+    addClickEvent(){
+        this.addClickEvent(clik)
+    }
+
+}
